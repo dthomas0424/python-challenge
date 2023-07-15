62f68264a3c74d90f67a66b5ca2d0c91ff1d8a88
dthomas0424 <dthomas0424@outlook.com>
2023-07-15 15:13:26 -0500

    initial poll file open

--- PYPoll/pypoll.py
+++ PYPoll/pypoll.py
@@ -1,5 +1,16 @@
 import os
 import csv
+#set lists
+#set variables
+
+#define CSV file location
+budget_data = os.path.join("..", "Resources", "election_data.csv")
+#open and read CSV file
+with open(budget_data) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=",")
+    #read file header
+    csv_header = next(csv_file)
+
 print("Election Results")
 print()
 print("---------------------------")
