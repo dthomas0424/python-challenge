4ee1e0b47ccedd2babc41252bdeac663026686e7
dthomas0424 <dthomas0424@outlook.com>
2023-07-15 16:07:47 -0500

    winner votes calculated

--- PYPoll/pypoll.py
+++ PYPoll/pypoll.py
@@ -1,6 +1,7 @@
 import os
 import csv
 #set lists
+candidate_votes = []
 #set variables
 total_votes = 0
 CCS_Votes = 0
@@ -9,6 +10,7 @@ RAD_Votes = 0
 CCS_percent = 0
 DD_percent = 0
 RAD_percent = 0
+winner_votes= 0
 
 #define CSV file location
 budget_data = os.path.join("..", "Resources", "election_data.csv")
@@ -34,7 +36,12 @@ with open(budget_data) as csv_file:
 CCS_percent = round(((CCS_Votes/total_votes) *100), 3)
 DD_percent = round(((DD_Votes/total_votes) *100), 3)
 RAD_percent = round(((RAD_Votes/total_votes) *100), 3)
+#add the vote numbers to a list in order to get the winner
+candidate_votes = [CCS_Votes, DD_Votes, RAD_Votes]
+#set the maximum number of votes as the winner
+winner_votes = max(candidate_votes)
 
+print(winner_votes)
 #print(f'{CCS_percent}%')
 #print(f'{DD_percent}%')
 #print(f'{RAD_percent}%')
