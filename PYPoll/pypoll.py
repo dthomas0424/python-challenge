ac1266c65cd0cb157df13730e19212c515e1e105
dthomas0424 <dthomas0424@outlook.com>
2023-07-15 15:50:10 -0500

    vote percentage calculations complete

--- PYPoll/pypoll.py
+++ PYPoll/pypoll.py
@@ -6,6 +6,9 @@ total_votes = 0
 CCS_Votes = 0
 DD_Votes = 0
 RAD_Votes = 0
+CCS_percent = 0
+DD_percent = 0
+RAD_percent = 0
 
 #define CSV file location
 budget_data = os.path.join("..", "Resources", "election_data.csv")
@@ -14,23 +17,34 @@ with open(budget_data) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
     #read file header
     csv_header = next(csv_file)
+    #loop through file and extract data
     for row in csv_reader:
+        #add up total number of votes
         total_votes += 1
+        #add up votes for CCS
         if row[2] == "Charles Casper Stockham":
             CCS_Votes += 1
+        #add up votes for DD    
         elif row[2] == "Diana DeGette":
             DD_Votes += 1
+        #add up votes for RAD    
         elif row[2] == "Raymon Anthony Doane":
             RAD_Votes += 1
-print(f'{CCS_Votes}')
-print(f'{DD_Votes}')
-print(f'{RAD_Votes}')
+#calculate percentage of votes for each candidate            
+CCS_percent = round(((CCS_Votes/total_votes) *100), 3)
+DD_percent = round(((DD_Votes/total_votes) *100), 3)
+RAD_percent = round(((RAD_Votes/total_votes) *100), 3)
+
+print(f'{CCS_percent}%')
+print(f'{DD_percent}%')
+print(f'{RAD_percent}%')
+
      
 print("Election Results")
 print()
 print("---------------------------")
 print()
-print("total Votes:")
+print(f"total Votes: {total_votes}")
 print()
 print("---------------------------")
 print()
