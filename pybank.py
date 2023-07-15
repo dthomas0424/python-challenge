a035fb7d97636557207dc0c12aa474d6e280b232
dthomas0424 <dthomas0424@outlook.com>
2023-07-15 12:00:55 -0500

    Totals completed

--- pybank.py
+++ pybank.py
@@ -1,19 +1,27 @@
 import os
 import csv
-
+#set variables
 month_count = 0
-
+month_PL = 0
+total_PL = 0
+#define CSV file location
 budget_data = os.path.join("..", "Resources", "budget_data.csv")
-
+#open and read CSV file
 with open(budget_data) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
-
+    #read file header
     csv_header = next(csv_file)
 
-
+    #loop through file to extract data
     for row in csv_reader:
-
+        #add up months in file
         month_count += 1
+        #add profit loss values from file
+        month_PL = int(row[1])
+        total_PL += month_PL
+
+
+
 
 
 
@@ -22,4 +30,5 @@ print()
 print('--------------------------------------')
 print()
 print(f'Total Months: {month_count}')
+print(f'Total: {total_PL}')
 
