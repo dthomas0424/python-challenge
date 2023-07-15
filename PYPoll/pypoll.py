507a9119be24798a72aaa0e55afa61b703ef095c
dthomas0424 <dthomas0424@outlook.com>
2023-07-15 16:48:13 -0500

    winner name calculated finally

--- PYPoll/pypoll.py
+++ PYPoll/pypoll.py
@@ -2,6 +2,7 @@ import os
 import csv
 #set lists
 candidate_votes = []
+candidate_name = []
 #set variables
 total_votes = 0
 CCS_Votes = 0
@@ -11,6 +12,8 @@ CCS_percent = 0
 DD_percent = 0
 RAD_percent = 0
 winner_votes= 0
+winner_name_index = 0
+winner_name = ""
 
 #define CSV file location
 budget_data = os.path.join("..", "Resources", "election_data.csv")
@@ -36,12 +39,24 @@ with open(budget_data) as csv_file:
 CCS_percent = round(((CCS_Votes/total_votes) *100), 3)
 DD_percent = round(((DD_Votes/total_votes) *100), 3)
 RAD_percent = round(((RAD_Votes/total_votes) *100), 3)
+#set winner_name to the candidate with the most votes
+if (CCS_Votes) > (DD_Votes) and (CCS_Votes) > (RAD_Votes):
+    winner_name = "Charles Casper Stockham"
+elif (DD_Votes) > (CCS_Votes) and (DD_Votes) > (RAD_Votes):
+    winner_name = "Diana DeGette"
+elif (RAD_Votes) > (DD_Votes) and (RAD_Votes) > (CCS_Votes):
+    winner_name = "Raymon Anthony Doane"
 #add the vote numbers to a list in order to get the winner
 candidate_votes = [CCS_Votes, DD_Votes, RAD_Votes]
 #set the maximum number of votes as the winner
 winner_votes = max(candidate_votes)
-
-print(winner_votes)
+#add candidate names to a list
+candidate_name = ["Charles Casper Stockham", "Diana DeGette", "Raymon Anthony Doane"]
+#get the winner name from the list by comparing it to the max votes
+winner_name_index = candidate_votes.index(winner_votes)
+#winner_name = candidate_name.index(winner_name_index)
+#print(winner_name)
+#print(winner_votes)
 #print(f'{CCS_percent}%')
 #print(f'{DD_percent}%')
 #print(f'{RAD_percent}%')
@@ -61,6 +76,6 @@ print(f"Anthony Doane: {RAD_percent}% {RAD_Votes}")
 print()
 print("---------------------------")
 print()
-print("Winner:")
+print(f"Winner: {winner_name}")
 print()
 print("---------------------------")
